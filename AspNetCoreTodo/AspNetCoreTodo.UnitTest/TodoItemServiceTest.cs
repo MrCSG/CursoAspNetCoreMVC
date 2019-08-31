eb0730adadf52c846cb9b52ad3e7c5d6a5ef8d0d
MrCSG <50384291+MrCSG@users.noreply.github.com>
2019-08-31 16:47:48 -0300

    TodoItemServiceTests unitarios en clase y tarea hechos

--- AspNetCoreTodo/AspNetCoreTodo.UnitTest/TodoItemServiceTest.cs
+++ AspNetCoreTodo/AspNetCoreTodo.UnitTest/TodoItemServiceTest.cs
@@ -1,3 +1,10 @@
+/* The MarkDoneAsync() method returns false if it's passed an ID that
+doesn't exist
+The MarkDoneAsync() method returns true when it makes a valid
+item as complete
+The GetIncompleteItemsAsync() method returns only the items
+owned by a particular user */
+
 using System;
 using System.Threading.Tasks;
 using AspNetCoreTodo.Data;
@@ -32,6 +39,7 @@ namespace AspNetCoreTodo.UnitTest
 
                 await service.AddItemAsync(todoItem, fakeUser);
             }
+
             using (var context = new ApplicationDbContext(options))
             {
                 var itemsInDatabase = await context.Items.CountAsync();
@@ -43,6 +51,117 @@ namespace AspNetCoreTodo.UnitTest
 
                 var difference = DateTimeOffset.Now.AddDays(3) - item.DueAt;
                 Assert.True(difference < TimeSpan.FromSeconds(1));
+            }
+        }
+        //The MarkDoneAsync() method returns false if it's passed an ID that doesn't exist
+        [Fact]
+        public async Task GivenAnInexistentID_MarkDoneAsync_ShouldReturnFalse()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_MarkDone_Failed").Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new ApplicationUser{
+                    Id = "fake-000",
+                    UserName = "fake@test.com"
+                };
+
+                var fakeTodoItem = new TodoItem{
+                    Title = "TestItem_MarkDoneFailed"
+                };
+
+                await service.AddItemAsync(fakeTodoItem, fakeUser);
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+                var items = await context.Items.ToArrayAsync();
+                var fakeUser1 = new ApplicationUser{
+                    Id = "fake-001",
+                    UserName = "fake1@test.com"
+                };
+
+                Assert.Equal(1, items.Length);
+                Assert.Equal("TestItem_MarkDoneFailed", items[0].Title);
+                Assert.Equal(false, items[0].IsDone);
+                Assert.Equal(false, await service.MarkDoneAsync(items[0].Id, fakeUser1));
+            }
+        }
+        //The MarkDoneAsync() method returns true when it makes a valid item as complete
+        [Fact]
+        public async Task GivenAValidItem_MarkDoneAsync_ShouldReturnTrue()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_MarkDone_Passed").Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new ApplicationUser{
+                    Id = "fake-000",
+                    UserName = "fake@test.com"
+                };
+
+                var fakeTodoItem = new TodoItem{
+                    Title = "TestItem_MarkDonePassed"
+                };
+
+                await service.AddItemAsync(fakeTodoItem, fakeUser);
+            }
+        
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+                var items = await context.Items.ToArrayAsync();
+                var fakeUser = new ApplicationUser{
+                    Id = "fake-000",
+                    UserName = "fake@test.com"
+                };
+
+                Assert.Equal(1, items.Length);
+                Assert.Equal("TestItem_MarkDonePassed", items[0].Title);
+                Assert.Equal(false, items[0].IsDone);
+                Assert.Equal(true, await service.MarkDoneAsync(items[0].Id, fakeUser));
+            }
+        }
+        //The GetIncompleteItemsAsync() method returns only the items owned by a particular user
+        [Fact]
+        public async Task GivenAnUser_GetIncompleteItemsAsync_ShouldReturnItemsOwnedByUser()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_GetIncompleteItems").Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new ApplicationUser{
+                    Id = "fake-000",
+                    UserName = "fake@test.com"
+                };
+
+                var fakeTodoItem = new TodoItem{
+                    Title = "TestItem_GetIncompleteItems"
+                };
+
+                await service.AddItemAsync(fakeTodoItem, fakeUser);
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+                var fakeUser = new ApplicationUser{
+                    Id = "fake-000",
+                    UserName = "fake@test.com"
+                };
+                var items = await service.GetIncompleteItemsAsync(fakeUser);
+
+                Assert.Equal(1, items.Length);
+                Assert.Equal("TestItem_GetIncompleteItems", items[0].Title);
+                Assert.Equal(false, items[0].IsDone);
+                Assert.Equal("fake-000", items[0].UserId);
             }
         }
     }
